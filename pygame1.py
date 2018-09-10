739d52af8e84d18229003e8da054948cdab297b8
Ritik Raj <33518732+chronos25@users.noreply.github.com>
2018-09-10 21:25:00 +0530

    rattlesnake
    using python 2.7 library pygame used , a rectangle is there on window  & has a function to move left & right. using libraries function event
    that identifies which key is pressed & decides what to happen next like event.type == pygame.KEYDOWN then it has two function either going left or going down by event.key== pygame.K_LEFT or event.key== pygame.K_RIGHT.

---
+++ pygame1.py
@@ -0,0 +1,50 @@
+import pygame
+
+pygame.init() #initialize
+#print x
+white=(255,255,255)
+black=(0,0,0)
+red=(255,0,0)
+
+
+gameDisplay =pygame.display.set_mode((800,600))
+#game display is object &
+pygame.display.set_caption('RATTLE SNAKE')
+pygame.display.update() #pygame.display.flip() are same with some
+#difference
+gameExit= False
+
+
+lead_x =300
+lead_y =300
+lead_x_change =0
+clock=pygame.time.Clock()
+
+#game loop>..............
+
+while not gameExit:
+    for event in pygame.event.get():
+        #print(event)
+        if event.type == pygame.QUIT:
+            gameExit = True #this makes exit through script itself
+        if event.type == pygame.KEYDOWN:
+            if event.key== pygame.K_LEFT:
+                lead_x_change-=10
+            if event.key== pygame.K_RIGHT:
+                lead_x_change+=10
+        if event.type ==pygame.KEYUP:
+            if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
+                lead_x_change=0
+        
+
+
+    lead_x+=lead_x_change
+    gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, black,[lead_x,lead_y,10,10])
+    pygame.display.update()
+
+    clock.tick(10) #human eye can notice
+
+
+pygame.quit()
+quit()
